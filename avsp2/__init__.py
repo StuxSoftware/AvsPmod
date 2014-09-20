fdbd0d039d0f061df82aa377bef0ed33625b3175
StuxSoftware <netzmit@gmail.com>
2014-09-20 11:56:47 +0200

    Replaced __author__ with header for avsp-mod files.

--- avsp2/__init__.py
+++ avsp2/__init__.py
@@ -1,1 +1,41 @@
-__author__ = 'Stux'
+# AvsP - an AviSynth editor
+#
+# Copyright 2007 Peter Jang <http://www.avisynth.org/qwerpoi>
+#           2010-2014 the AvsPmod authors <https://github.com/avspmod/avspmod>
+#
+# Printing support based on stcprint.py from Peppy/Editra (wxWidgets license)
+# Copyright 2007 Cody Precord <staff@editra.org>
+#           2009 Rob McMullen <robm@users.sourceforge.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+#  http://www.gnu.org/copyleft/gpl.html .
+
+# Dependencies:
+#     Python (tested on v2.6 and 2.7)
+#     wxPython (tested on v2.8 Unicode and 2.9)
+#     cffi and its dependencies (only for x86-64, tested on v0.8.1)
+#         pycparser
+#         Visual Studio 2008
+#     avisynth_c.h (only for x86-64, interface 5, or at least 3 + colorspaces
+#                   from 5, tested with the header used by x264)
+# Scripts:
+#     wxp.py (general wxPython framework classes)
+#     avisynth.py (Python AviSynth/AvxSynth wrapper, only for x86-32)
+#     avisynth_cffi.py (Python AviSynth wrapper, only for x86-64)
+#     pyavs.py (AvsP AviSynth support by loading AviSynth directly as a library)
+#     pyavs_avifile.py (AvsP AviSynth support through Windows AVIFile routines)
+#     icon.py (icons embedded in a Python script)
+#     i18n.py (internationalization and localization)
+#     global_vars.py (application info and other shared variables)
